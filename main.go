8f31186da96f200a66a4932f35445c483f0b5eb3
Oliver Westenra <oliver.westenra@hotmail.co.nz>
2023-12-17 00:38:25 +1300

    pulling actual data

--- main.go
+++ main.go
@@ -6,8 +6,15 @@ import (
 	"net/http/cookiejar"
 	"net/url"
 	"os"
+	"strings"
 
 	"golang.org/x/net/html"
+)
+
+var (
+	ownedCards   []string
+	cardsInStock []string
+	missingCards []string
 )
 
 func login(client *http.Client, username, password string) error {
@@ -59,7 +66,7 @@ func getCollectableCardShopStock(client *http.Client) (*html.Node, error) {
 }
 
 // Need to use the golang.org/x/net/html package to parse the HTML and find the list of cards
-func extractItemNamesAndQuantities(node *html.Node) {
+func extractItemNames(node *html.Node) {
 	var crawler func(*html.Node)
 	crawler = func(n *html.Node) {
 		// Check if the node is an <img> tag
@@ -69,9 +76,50 @@ func extractItemNamesAndQuantities(node *html.Node) {
 				if sibling.Type == html.ElementNode && sibling.Data == "b" {
 					// Extract and print the text inside the <b> tag
 					if sibling.FirstChild != nil {
-						fmt.Println("Card Name:", sibling.FirstChild.Data)
+						ownedCards = append(ownedCards, sibling.FirstChild.Data)
+						// fmt.Println("Card Name:", sibling.FirstChild.Data)
 					}
 					break
+				}
+			}
+		}
+		for child := n.FirstChild; child != nil; child = child.NextSibling {
+			crawler(child)
+		}
+	}
+	crawler(node)
+}
+
+func extractDataNames(node *html.Node) {
+	var crawler func(*html.Node)
+	crawler = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "div" {
+			// Check if this is a 'shop-item' div
+			isShopItem := false
+			for _, a := range n.Attr {
+				if a.Key == "class" && strings.Contains(a.Val, "shop-item") {
+					isShopItem = true
+					break
+				}
+			}
+
+			if isShopItem {
+				// Find the 'item-img' div inside this 'shop-item' div
+				for child := n.FirstChild; child != nil; child = child.NextSibling {
+					if child.Type == html.ElementNode && child.Data == "div" {
+						for _, a := range child.Attr {
+							if a.Key == "class" && strings.Contains(a.Val, "item-img") {
+								// Extract and print the 'data-name' attribute
+								for _, a := range child.Attr {
+									if a.Key == "data-name" {
+										cardsInStock = append(cardsInStock, a.Val)
+										// fmt.Println("Data Name:", a.Val)
+										break
+									}
+								}
+							}
+						}
+					}
 				}
 			}
 		}
@@ -92,9 +140,31 @@ func getAttributeValue(node *html.Node, attrName string) string {
 	return ""
 }
 
+// contains checks if slice1 contains all elements of slice2.
+func contains(slice1, slice2 []string) []string {
+	for _, val2 := range slice2 {
+		found := false
+		for _, val1 := range slice1 {
+			if val1 == val2 {
+				found = true
+				break
+			}
+		}
+		if !found {
+			missingCards = append(missingCards, val2)
+		}
+	}
+	return missingCards
+}
+
 func main() {
 	jar, _ := cookiejar.New(nil)
 	client := &http.Client{Jar: jar}
+
+	if len(os.Args) == 0 {
+		fmt.Println("No username or password passed in. Aborting")
+		return
+	}
 
 	username := os.Args[1]
 	password := os.Args[2]
@@ -104,21 +174,35 @@ func main() {
 		fmt.Println("Login failed:", err)
 		return
 	}
+	fmt.Println("Login Successful")
 
-	getCollectableCardShopStock(client)
+	fmt.Println("Retrieving card shop stock")
+	shopStock, err := getCollectableCardShopStock(client)
+	if err != nil {
+		fmt.Println("Failed to get Card shop stock:", err)
+		return
+	}
+	fmt.Println("Card shop stock received")
+
+	fmt.Println("Retrieving my Neodeck cards")
+	neodeckCards, err := getNeodeck(client, username)
 	if err != nil {
 		fmt.Println("Failed to get Neodeck:", err)
 		return
 	}
+	fmt.Println("Neodeck retrieved")
 
-	//neodeckPage, err := getNeodeck(client, username)
-	// if err != nil {
-	// 	fmt.Println("Failed to get Neodeck:", err)
-	// 	return
-	// }
+	// Functions for reading locally
 
 	// // Reading in a file for testing so we aren't logging in each time we run it
 	// neodeckPage, err := os.Open("output.html")
+	// if err != nil {
+	// 	fmt.Errorf("Failed to read file:", err)
+	// 	return
+	// }
+	// defer neodeckPage.Close() // closes the file after everything is done
+
+	// shopPage, err := os.Open("shop.html")
 	// if err != nil {
 	// 	fmt.Errorf("Failed to read file:", err)
 	// 	return
@@ -131,6 +215,18 @@ func main() {
 	// 	return
 	// }
 
-	// extractItemNamesAndQuantities(doc)
-	//fmt.Println("Cards in Neodeck:", doc)
+	// shopDoc, err := html.Parse(shopPage)
+	// if err != nil {
+	// 	fmt.Errorf("Failed to parse file:", err)
+	// 	return
+	// }
+
+	extractItemNames(neodeckCards)
+	extractDataNames(shopStock)
+
+	fmt.Println("Missing cards currently in stock:")
+	for _, card := range missingCards {
+		fmt.Println(card)
+	}
+
 }
